fc2572b85d544cf3e1e8c346c65ea2e3e753c53c
laksamana-97 <73052991+laksamana-97@users.noreply.github.com>
2020-11-10 03:26:07 +0700

    Add files via upload

---
+++ src/menu.js
@@ -0,0 +1,23 @@
+module.exports.menu = `Hai 🙋🏻‍♂️, selamat datang di 'MD Bot 🤖', ini daftar menu yang bisa kalian pakai
+
+*#sticker*
+_Bikin Stiker dari Gambar 🖼, kirim gambar dan beri caption atau bisa juga quote/reply dari gambar yang sudah ada di chat/group_
+
+*#korona*
+_Data Korona Indonesia 🦠, data realtime dan terupdate dari sumber terpercaya_
+  
+*#quotes*
+_Random Quotes Bahasa Indonesia 🔖_
+
+*#wp*
+_Random Wallpaper Cantik 🌈_
+
+*#zodiak <nama> <tgl>*
+_Cek Zodiak Kamu 🐟_
+_(contoh: #zodiak anya 11-11-1991)_
+
+*#ramalan <nama> <tgl> <nama> <tgl>*
+_Ramalan Kecocokan Cinta dengan Pasangan ❤️_
+_(contoh: #ramalan andi 11-11-1991 anggi 11-11-1991)_
+  
+** Donasi kesini ya https://saweria.co/mukhlis, biar aku bisa tetep hidup 🤖`;
